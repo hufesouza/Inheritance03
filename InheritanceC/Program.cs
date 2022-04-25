a999e9c088e1614144ec05068c28b79269c2ffa8
Hugo Ferreira <hugof@Hugos-MacBook-Air.local>
2022-04-25 23:04:27 +0100

    Adding timer adn VideoPost class

--- InheritanceC/Program.cs
+++ InheritanceC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace InheritanceC
 {
@@ -11,6 +12,18 @@ namespace InheritanceC
 
             ImagePost imagePost1 = new ImagePost("Check out my new shoes by clicking on the following link", "Hugo Ferreira", "http://images.com/shoes", true);
             Console.WriteLine(imagePost1.ToString());
+
+            VideoPost videoPost1 = new VideoPost("Check out this new video", "Hugo Ferreira", "http://video.com/post1", true, 12);
+            Console.WriteLine(videoPost1.ToString());
+
+            videoPost1.Play();
+            Console.WriteLine("Press any key to stop the video!");
+            Console.ReadKey();
+            videoPost1.Stop();
+            Console.ReadLine();
+
+
         }
+
     }
 }
---
+++ InheritanceC/VideoPost.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InheritanceC
+{
+    public class VideoPost:Post
+    {
+        protected bool isPlaying = false;
+        protected int currDuration = 0;
+        Timer timer;
+
+        protected string VideoURL { get; set; }
+        protected int Length { get; set; }
+
+        public VideoPost() { }
+        public VideoPost(string title, string sendByUsername, string videoURL, bool isPublic, int length)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+
+            this.VideoURL = videoURL;
+            this.Length = length;
+        }
+
+        public void Play()
+        {
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                Console.WriteLine("Is Playing");
+                timer = new Timer(TimerCallback, null, 0, 1000);
+            }
+        }
+
+        private void TimerCallback(Object o)
+        {
+            if (currDuration < Length)
+            {
+                currDuration++;
+                Console.WriteLine($"Video at {currDuration}");
+                GC.Collect();
+            }
+        }
+
+        public void Stop()
+        {
+            isPlaying = false;
+            Console.WriteLine($"Video stopped at {currDuration}");
+            currDuration = 0;
+            timer.Dispose();
+
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{this.ID} - {this.Title} - {this.VideoURL} - by {this.SendByUsername}- {this.Length}");
+        }
+
+    }
+}
